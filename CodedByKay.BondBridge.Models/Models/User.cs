5079db9864c13b2fba0befa73c54aab806d6ba70
Kay Wiberg <kaywib@gmail.com>
2024-02-20 20:19:22 +0100

    Added image path to user class

--- CodedByKay.BondBridge.Models/Models/User.cs
+++ CodedByKay.BondBridge.Models/Models/User.cs
@@ -3,6 +3,7 @@
     public class User
     {
         public Guid UserId { get; set; }
+        public string ImagePath{ get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public ICollection<Group> Groups { get; set; }
