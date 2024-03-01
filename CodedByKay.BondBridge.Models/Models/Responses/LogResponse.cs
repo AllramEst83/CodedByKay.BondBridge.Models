b755db7691be7dbc9abbf58ca4de3153f7aea4a6
Kay Wiberg <kaywib@gmail.com>
2024-03-01 14:50:48 +0100

    Added LogModels

---
+++ CodedByKay.BondBridge.Models/Models/Responses/LogResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace CodedByKay.BondBridge.Models.Models.Responses
+{
+    public class LogResponse
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string StackTrace { get; set; }
+    }
+
+}
